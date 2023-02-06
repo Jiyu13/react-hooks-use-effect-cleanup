2126bed4278adcec4b3fccc0b6a6f46a7d1a253f
Ziru Zou <jiyucoding@yahoo.com>
2023-02-05 23:56:12 -0500

    code along

--- src/Clock.js
+++ src/Clock.js
@@ -1,12 +1,23 @@
 import React, { useState, useEffect } from "react";
 
+// 1. a component that runs a timer in the bg continuously
 function Clock() {
   const [time, setTime] = useState(new Date());
 
+  // 
   useEffect(() => {
-    setInterval(() => {
+    // 4. setInterval()'s still running in the bg even this component is removed from DOM
+    // 6. assign setInterval() to a variable
+    const timerID = setInterval(() => {
       setTime(new Date());
-    }, 1000);
+    }, 1000); 
+
+    // 5. return a cleanup function to stop setInterval, 
+    // clearInterval(intervalID) will cancel setInterval()
+    return function cleanup() {
+      clearInterval(timerID)
+    }
+
   }, []);
 
   return <div>{time.toString()}</div>;
